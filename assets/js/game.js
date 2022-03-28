d76c07f6fc7878ba8ed1f52ad2e7715b0ffd1196
SD-github21 <sapandonde.gmail.com>
2022-03-28 08:36:28 -0500

    add multiple rounds until defeated with while loop

--- assets/js/game.js
+++ assets/js/game.js
@@ -21,9 +21,9 @@ var enemyAttack = 12;
 
 
 var fight = function(enemyName) {
-    // Alert players that they are starting the round
-    window.alert("Welcome to the Robot Gladiators!");
-
+  //repeat and execute as long as the enemy robot is alive
+  while(enemyHealth > 0) {
+ 
     // Determine if the player would like to fight or skip the battle
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
@@ -74,10 +74,15 @@ var fight = function(enemyName) {
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }
-};
+ };
+
+}
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    //call fight function with enemy robot
+    fight(pickedEnemyName);
 }
 
 
